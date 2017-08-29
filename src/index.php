3bf7e8e764f9be7fb7046c5e232f57820e12d2a2
Jakub Rusinowicz <jakub.rusinowicz@coderslab.pl>
2017-08-29 13:46:32 +0200

    index

--- src/index.php
+++ src/index.php
@@ -6,18 +6,6 @@ $username = "test";
 $password = "test";
 $db = 'test';
 
-//    $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
 
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-try {
-
-
-    $sql = 'CREATE TABLE test';
-    $conn->query($sql);
-    echo "New table created successfully";
-}
-catch(PDOException $e)
-{
-    echo $sql . "<br>" . $e->getMessage();
-}
 
